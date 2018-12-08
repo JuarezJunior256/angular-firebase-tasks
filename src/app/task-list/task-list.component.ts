bc0072286bdfdbcbaa7d55733f5137d6a32176c6
JuarezJunior256 <juarezjunior.live@gmail.com>
2018-12-08 18:19:26 -0300

    Adicionando component lit

---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../models/task.model';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.scss']
+})
+export class TaskListComponent implements OnInit {
+
+  task: Task[] = [
+    {
+      uid: '',
+      title: 'Curso Anguar',
+      done: false
+    },
+    {
+      uid: '',
+      title: 'Curso JS',
+      done: false
+    }
+  ];
+
+  selectedTask: Task;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+    onPerformTask(task: Task): void {
+     console.log(task);
+   }
+
+}
